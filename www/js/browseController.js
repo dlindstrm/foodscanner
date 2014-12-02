a3ae7e78ff352566cbf3ef1c1379e229eccfceb2
Daniel Lindström <d.lindstrom@live.com>
2014-12-02 23:39:35 +0100

    browse & favorite

--- www/js/browseController.js
+++ www/js/browseController.js
@@ -1,9 +1,11 @@
 angular.module('foodscan.browseController', [])
 
-.controller("BrowseController", function($scope, $http, ArticleList) {
+.controller("BrowseController", function($scope, $http, ArticleList, Favorite) {
   $scope.levelOne = [];
   $scope.levelTwo = [];
   $scope.levelThree = [];
+  $scope.selected1 = false;
+  $scope.selected2 = false;
 
   $http.get('http://fsserver.kspri.se/api/get/category?level=1')
   .success(function(data, status) {
@@ -13,12 +15,22 @@ angular.module('foodscan.browseController', [])
  $scope.setLvl = function(cat1, cat2) {
   var url = 'http://fsserver.kspri.se/api/get/category?cat1=';
   
-  if(cat2 == undefined) {
+  if(cat1 == undefined) {
+    $scope.selected1 = false;
+    $scope.selected2 = false;
+    $scope.levelTwo = [];
+    $scope.levelThree = [];
+  }
+
+  else if(cat2 == undefined) {
+    $scope.selected1 = false;
+    $scope.selected2 = false;
     $scope.levelTwo = [];
     $scope.levelThree = [];
     $http.get(url+cat1)
       .success(function(data, status) {
             $scope.levelTwo = data.childs;
+            $scope.selected1 = true;
       });
   }
 
@@ -26,6 +38,7 @@ angular.module('foodscan.browseController', [])
     $http.get(url+cat1+'&cat2='+cat2)
       .success(function(data, status) {
             $scope.levelThree = data.childs;
+            $scope.selected2 = true;
       });
  }
 
@@ -37,10 +50,9 @@ angular.module('foodscan.browseController', [])
     ArticleList.goTo('http://fsserver.kspri.se/api/get/article?cat1='+cat1+'&cat2='+cat2);
   }
   else {
-    console.log('http://fsserver.kspri.se/api/get/article?cat1='+cat1+'&cat2='+cat2+'&cat3'+cat3);
     ArticleList.goTo('http://fsserver.kspri.se/api/get/article?cat1='+cat1+'&cat2='+cat2+'&cat3='+cat3);
   }
 }
-
 }
+
 });
--- www/js/favoriteController.js
+++ www/js/favoriteController.js
@@ -1,5 +1,14 @@
 angular.module('foodscan.favoriteController', [])
 
-.controller("FavoriteController", function($scope) {
+.controller("FavoriteController", function($scope, Articles, Favorite) {
 	$scope.favs = JSON.parse(localStorage["favorites"]);
+
+	this.goto = function(gtin) {
+	   Articles.goTo(gtin);
+	}
+
+	$scope.toggleFav = function(id) {
+    	$scope.favorite = Favorite.toggleFavorite(id);
+    	$scope.favs = JSON.parse(localStorage["favorites"]);
+  	}
 });
